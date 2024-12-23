df82603facc0270e4e6eba267b4c6e05fe3f5389
Daniel Brannock <mbrannock@rti.org>
2024-12-23 14:54:25 -0500

    Add a license (proprietary for now) and add a function to check GitHub Actions parsing

---
+++ R/placeholder.R
@@ -0,0 +1,10 @@
+#' Placeholder function
+#'
+#' Evaluate if functions are being processed correctly by R CMD CHECK.
+#'
+#' @param input
+#'
+#' @return 0
+return_0 <- function(input) {
+  return(0)
+}
