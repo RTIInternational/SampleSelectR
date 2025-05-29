d680422140f1ee4cead9af78b278e255f1aed9cc
Sow, Haby <habysow@rti.org>
2025-05-29 10:08:27 -0400

    Initial systematic function (#9)
    * done!
    
    * d
    
    * adding documentation
    
    * Add runif to import, add ProjectID back to rproj
    
    * With feedback
    
    * reviewed fedback
    
    * feedback change
    
    * sys done
    
    ---------
    
    Co-authored-by: Stephanie Zimmer <sazimmer@rti.org>

--- R/SampleSelectR-package.R
+++ R/SampleSelectR-package.R
@@ -11,6 +11,7 @@
 #' @importFrom data.table .SD
 #' @importFrom data.table :=
 #' @importFrom data.table data.table
+#' @importFrom stats runif
 #' @importFrom rlang .data
 ## usethis namespace: end
 NULL
