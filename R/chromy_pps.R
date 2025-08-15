d86411cddc66218e9e3ae29ccb1044a134424218
Stephanie Zimmer <sazimmer@rti.org>
2025-08-14 22:54:39 -0400

    styler added

--- R/chromy_pps.R
+++ R/chromy_pps.R
@@ -13,53 +13,52 @@
 #' @param curstrat A character variable that specifies the current strata, only used as an assertion for the n == N test.
 #'
 #' @return Returns an object of type tidytable that contains the weight, selection probability, number of hits, etc plus all original variables.
-chromy_pps <- function(frame, n, mos, outall=FALSE, curstrat=NULL){
+chromy_pps <- function(frame, n, mos, outall = FALSE, curstrat = NULL) {
   # TODO: Add in asserts
 
   N <- nrow(frame)
-  mosv <- n*frame[[mos]]/sum(frame[[mos]])
+  mosv <- n * frame[[mos]] / sum(frame[[mos]])
   moscuml <- cumsum(mosv)
   I <- floor(moscuml)
-  F <- moscuml-I
+  F <- moscuml - I
 
-  chromy_inner <- function(F, I){
+  chromy_inner <- function(F, I) {
     N <- length(F)
     hits <- rep(0, N)
     r <- runif(N)
 
-    for (i in seq_len(N)){
-      if (i==1L){
+    for (i in seq_len(N)) {
+      if (i == 1L) {
         Fprev <- 0.0
         Iprev <- 0.0
-      } else{
-        Fprev <- F[i-1]
-        Iprev <- I[i-1]
+      } else {
+        Fprev <- F[i - 1]
+        Iprev <- I[i - 1]
       }
       PriorSum <- sum(hits)
 
-      if (F[i]==0){ # Condition 1
+      if (F[i] == 0) { # Condition 1
         hits[i] <- 0
-      } else if (F[i] > Fprev){ # Condition 2
-        if (PriorSum==Iprev){ # First column
-          if (r[i] < (F[i]-Fprev)/ (1-Fprev)){
-            hits[i] <- I[i] +1- PriorSum
-          } else{
-            hits[i] <- I[i]-PriorSum
+      } else if (F[i] > Fprev) { # Condition 2
+        if (PriorSum == Iprev) { # First column
+          if (r[i] < (F[i] - Fprev) / (1 - Fprev)) {
+            hits[i] <- I[i] + 1 - PriorSum
+          } else {
+            hits[i] <- I[i] - PriorSum
           }
-        } else{ # Second column
-          hits[i] <- I[i] + 1-PriorSum
+        } else { # Second column
+          hits[i] <- I[i] + 1 - PriorSum
         }
-      } else{ # Condition 3
-        if (PriorSum==Iprev){ # First column
-          hits[i] <- I[i]-PriorSum
-        } else{ # Second column
-          if (r[i] < F[i]/Fprev){
-            hits[i] <- I[i] +1- PriorSum
-          } else{
-            hits[i] <- I[i]- PriorSum
+      } else { # Condition 3
+        if (PriorSum == Iprev) { # First column
+          hits[i] <- I[i] - PriorSum
+        } else { # Second column
+          if (r[i] < F[i] / Fprev) {
+            hits[i] <- I[i] + 1 - PriorSum
+          } else {
+            hits[i] <- I[i] - PriorSum
           }
         }
-
       }
     }
     hits
@@ -71,17 +70,16 @@ chromy_pps <- function(frame, n, mos, outall=FALSE, curstrat=NULL){
       ExpectedHits = mosv,
       NumberHits = chromy_inner(F, I),
       SelectionIndicator = .data$NumberHits > 0,
-      SamplingWeight = ifelse(.data$SelectionIndicator, 1/.data$ExpectedHits, NA),
+      SamplingWeight = ifelse(.data$SelectionIndicator, 1 / .data$ExpectedHits, NA),
     )
 
-  if (outall){
+  if (outall) {
     return(frame_hits)
-  } else{
+  } else {
     sample_out <- frame_hits |>
       tidytable::filter(.data$SelectionIndicator) |>
       tidytable::select(-.data$SelectionIndicator)
 
     return(sample_out)
   }
-
 }
