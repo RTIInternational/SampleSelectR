a112a33b93afd274d909971b3bd523ce2600ca6e
Zimmer, Stephanie <sazimmer@rti.org>
2025-09-05 09:50:50 -0400

    Loop through strata using map and improve strata comparison (#27)
    * Loop through strata using map and improve strata comparison
    
    * Added all_of() to tidytable::select
    
    ---------
    
    Co-authored-by: Burkett, Thomas <tburkett@rti.org>

--- R/select_sample.R
+++ R/select_sample.R
@@ -30,8 +30,7 @@
 #'
 #' @export
 
-select_sample <- function(frame, method, n, outall=FALSE, strata=NULL, mos=NULL, sort_vars=NULL, sort_method=NULL)
-{
+select_sample <- function(frame, method, n, outall = FALSE, strata = NULL, mos = NULL, sort_vars = NULL, sort_method = NULL) {
   #### Begin Assertion Checks ####
 
   check_frame_type(frame)
@@ -81,44 +80,93 @@ select_sample <- function(frame, method, n, outall=FALSE, strata=NULL, mos=NULL,
         !(inherits(n, "tibble"))) {
       stop("Since strata is not NULL, n must be a data frame, data.table, or tibble.")
     }
+
+
     # Check that there is a 1:1 correspondence between the distinct strata on frame and the values on the sample size table, except for sample_size
     if (!all(strata %in% names(n))) {
       stop("Since strata is not NULL, all strata variables must be present on the sample size table, n.")
     }
-    for (strati in strata)
-    {
-      # Get unique values and sort
-      check_frame <- sort(unique(frame[[strati]]))
-      check_n <- sort(unique(n[[strati]]))
-      # Compare and return error if unique values for stratum are not equal
-      if (all(class(check_frame) != class(check_n))){
-        stop(paste0("The class for strata ", strati, " on the sampling frame and the sample size table are not the same."))
+
+    get_distinct_str <- function(dat){
+      dat |>
+        tidytable::distinct(tidytable::all_of(strata)) |>
+        tidytable::select(tidytable::all_of(strata))
+    }
+
+    check_frame <- get_distinct_str(frame)
+    check_n <- get_distinct_str(n)
+
+    class_frame <- lapply(check_frame, class) |> unlist()
+    class_n <- lapply(check_n, class) |> unlist()
+
+    if (nrow(n) != nrow(check_n)) {
+      stop("There are repeated strata values on the sample size table. Must be unique and no repeats.")
+    }
+
+    if (!all(class_frame == class_n)){
+      diffc <- names(class_frame)[which(class_frame != class_n)] |>
+        paste(collapse=", ")
+      stop(paste0("The class for strata ", diffc, " on the sampling frame and the sample size are not the same."))
+    }
+
+    check_notn <- check_frame |>
+      tidytable::anti_join(check_n, by=strata)
+    check_notframe <- check_n |>
+      tidytable::anti_join(check_frame, by=strata)
+
+    if (nrow(check_notframe) >0 || nrow(check_notn) >0){
+      make_mismatch_text <- function(dat){
+        dat |>
+          tidytable::mutate(row=tidytable::row_number()) |>
+          tidytable::pivot_longer(cols=-row) |>
+          tidytable::mutate(txt=paste(.data$name, .data$value, sep="=")) |>
+          tidytable::summarise(
+            txt=paste(.data$txt, collapse=", "),
+            .by="row"
+          ) |>
+          tidytable::summarise(
+            txt=paste(.data$txt, collapse=";")
+          ) |>
+          tidytable::pull(.data$txt)
       }
-      if (all(check_frame != check_n)) {
-        stop(paste0("Unique values of strata ", strati, " do not match, which may cause errors later in sampling. Please investigate."))
+
+      txtframe <- txtn <- NULL
+      if (nrow(check_notframe) >0){
+        txtframe <- make_mismatch_text(check_notframe)
+      }
+      if (nrow(check_notn) > 0){
+        txtn <- make_mismatch_text(check_notn)
+      }
+
+      if (!is.null(txtframe) && !is.null(txtn)){
+        stop(paste("There are strata values on frame but not sample (", txtn, ") and values on sample but not frame (", txtframe, ")."))
+      } else if (!is.null(txtframe)){
+        stop(paste("There are strata values on sample but not frame (", txtframe, ")."))
+      } else if (!is.null(txtn)){
+        stop(paste("There are strata values on frame but not sample (", txtn, ")."))
       }
     }
+
     # Check that sample_size is present on the sample size table, n
     if (!("sample_size" %in% names(n))) {
       stop("Since strata is not NULL, the sample size table, n,  must have a column named 'sample_size'.")
-    }
-    # Check that sample_size is a positive integer for each value
-    if (!(is.numeric(n$sample_size)) || min(n$sample_size) <= 0 || sum(round(n$sample_size)) != sum(n$sample_size) || sum(is.na(n$sample_size)) > 0) {
-      stop("Since strata is not NULL, each value of sample_size must be a positive, non-missing integer.")
     }
     # Additionally, check that there are no missing values on entire n frame
     if (any(is.na(n$sample_size))) {
       stop("Since strata is not NULL, the sample size table, n, must not have any missing values.")
     }
-
-    if (n |> tidytable::group_by(strata) |> tidytable::filter(tidytable::n() >1) |> nrow() >1){
-      stop("There are repeated strata values on the sample size table. Must be unique and no repeats.")
+    # Check that sample_size is a positive integer for each value
+    if (!(is.numeric(n$sample_size)) || min(n$sample_size) <= 0 || sum(round(n$sample_size)) != sum(n$sample_size) || sum(is.na(n$sample_size)) > 0) {
+      stop("Since strata is not NULL, each value of sample_size must be a positive, non-missing integer.")
     }
+
+
+
   }
 
 
   # Check for valid values of outall
-  if (length(outall) != 1 && !(inherits(outall, "logical"))) {
+  if (length(outall) != 1 && !is.logical(outall)) {
     stop("outall must be a single logical value of length 1.")
   }
   if (!(outall %in% c(TRUE, FALSE))) {
@@ -140,7 +188,7 @@ select_sample <- function(frame, method, n, outall=FALSE, strata=NULL, mos=NULL,
       stop("Since mos is not NULL, it must be a variable on the frame.")
     }
     # Check that mos is numeric and has no missing values
-    if (!(inherits(frame[[mos]], "numeric")) || sum(is.na(frame[[mos]])) > 0) {
+    if (!is.numeric(frame[[mos]]) || sum(is.na(frame[[mos]])) > 0) {
       stop("Since mos is not NULL, it must be numeric with no missing values.")
     }
   } else {
@@ -170,8 +218,7 @@ select_sample <- function(frame, method, n, outall=FALSE, strata=NULL, mos=NULL,
     if (method == "srs") {
       stop("Since sort_vars is not NULL, sampling method cannot be 'srs'; i.e., sampling method must be either 'sys_eq', 'sys_pps', or 'chromy_pps'.")
     }
-  }
-  else {
+  } else {
     # Check if sampling method is srs. If not, display message stating that no sorting will be done as there are no sorting variables
     if (method != "srs") {
       message("No sorting variables are provided so frame is assumed to be already sorted for systematic sampling.")
@@ -181,7 +228,7 @@ select_sample <- function(frame, method, n, outall=FALSE, strata=NULL, mos=NULL,
 
   # Check that sort_method is a character string of length 1
   if (!is.null(sort_method)) {
-    if (!is.character(sort_method) || length(method) != 1) {
+    if (!is.character(sort_method) || length(sort_method) != 1) {
       stop("The sorting method must be a character string of length 1.")
     }
     # Check that sort_method is valid sorting option
@@ -193,8 +240,7 @@ select_sample <- function(frame, method, n, outall=FALSE, strata=NULL, mos=NULL,
     if (is.null(sort_vars)) {
       stop("Since sort_method is not NULL, sort_vars must also not be NULL.")
     }
-  }
-  else {
+  } else {
     # Check that sort_vars is also NULL
     if (!is.null(sort_vars)) {
       stop("Since sort_method is NULL, sort_vars must also be NULL.")
@@ -206,16 +252,23 @@ select_sample <- function(frame, method, n, outall=FALSE, strata=NULL, mos=NULL,
   # Stratified Sampling
   if (is.null(strata)) {
     frame <- frame |>
-      tidytable::mutate(.strata=1)
+      tidytable::mutate(.strata = 1)
 
     nvec <- n
 
-    n <- data.frame(.strata=1, sample_size=nvec)
+    n <- data.frame(.strata = 1, sample_size = nvec) |>
+      tidytable::mutate(strlab = NA)
 
     strata <- ".strata"
     strataog <- NULL
-  } else{
+  } else {
     strataog <- strata
+    strnm <- tidytable::map(strata, ~ paste(.x, n[[.x]], sep = " = ")) |>
+      unlist() |>
+      matrix(nrow = length(strata), byrow = TRUE) |>
+      apply(2, paste, collapse = ", ")
+    n <- n |>
+      tidytable::mutate(strlab = strnm)
   }
 
   # First, split the frame into multiple strata frames if strata is not NULL
@@ -223,72 +276,60 @@ select_sample <- function(frame, method, n, outall=FALSE, strata=NULL, mos=NULL,
     frame |>
     tidytable::group_by(tidytable::all_of(strata)) |>
     tidytable::nest() |>
-    tidytable::full_join(n, by=strata) |>
+    tidytable::full_join(n, by = strata) |>
     tidytable::ungroup()
 
+  # Sort, if necessary
+  if (is.null(sort_method)) {
+    # do nothing
+  } else if (sort_method == "serpentine") {
+    strata_frames <- strata_frames |>
+      tidytable::mutate(data = tidytable::map(.data$data, ~ serp_sort(.x, sort_vars)))
+  } else if (sort_method == "nest") {
+    strata_frames <- strata_frames |>
+      tidytable::mutate(data = tidytable::map(.data$data, ~ tidytable::arrange(.x, tidytable::pick(sort_vars))))
+  }
 
-  for (i in seq_len(nrow(strata_frames))) {
+  # Finally, pass along to helper sampling function based on method
 
-    # Next, sort the frame within the stratum
-    if (is.null(sort_method)){
-      # No sorting
-      assign(paste0("sort", i), strata_frames$data[[i]])
+  inplist <- list(frame = strata_frames$data, n = strata_frames$sample_size, curstrat = strata_frames$strlab)
+
+  if (method == "srs") {
+    if (!is.null(strataog)) {
+      samptmp <- inplist |>
+        tidytable::pmap(.f = function(frame, n, curstrat) srs(frame, n, curstrat, outall = outall))
+    } else {
+      samptmp <- list(srs(inplist$frame[[1]], inplist$n, outall = outall))
     }
-    else if (sort_method == "serpentine") {
-      # Serpentine sorting
-      assign(paste0("sort", i), serp_sort(mydata=strata_frames$data[[i]], sort_vars))
+  } else if (method %in% c("sys_eq", "sys")) {
+    if (!is.null(strataog)) {
+      samptmp <- inplist |>
+        tidytable::pmap(.f = function(frame, n, curstrat) sys(frame, n, curstrat, outall = outall))
+    } else {
+      samptmp <- list(sys(inplist$frame[[1]], inplist$n, outall = outall))
     }
-    else if (sort_method == "nest") {
-      # Nest sorting
-      assign(paste0("sort", i), tidytable::arrange(mydata=strata_frames$data[[i]], sort_vars))
+  } else if (method == "sys_pps") {
+    if (!is.null(strataog)) {
+      samptmp <- inplist |>
+        tidytable::pmap(.f = function(frame, n, curstrat) sys_pps(frame, n, curstrat, outall = outall, mos = mos))
+    } else {
+      samptmp <- list(sys_pps(inplist$frame[[1]], inplist$n, outall = outall, mos = mos))
     }
-
-    # Next, identify the sample size for relevant stratum
-    sample_size_use <- strata_frames |>
-      tidytable::slice(i) |>
-      tidytable::pull(sample_size)
-
-    # Next, define curstrat (current stratum variable)
-    if (!is.null(strataog)){
-      strat_vals <- strata_frames |>
-        tidytable::slice(i) |>
-        tidytable::select(tidytable::all_of(strata))
-
-      curstrat <- paste(names(strat_vals), unlist(strat_vals), sep=" = ", collapse=", ")
-    } else{
-      curstrat <- NULL
+  } else if (method %in% c("chromy_pps", "seq_pps")) {
+    if (!is.null(strataog)) {
+      samptmp <- inplist |>
+        tidytable::pmap(.f = function(frame, n, curstrat) chromy_pps(frame, n, curstrat, outall = outall, mos = mos))
+    } else {
+      samptmp <- list(chromy_pps(inplist$frame[[1]], inplist$n, outall = outall, mos = mos))
     }
+  }
 
-    # Finally, pass along to helper sampling function based on method
-    if (method == "srs") {
-      samptmp <- srs(frame=get(paste0("sort", i)), n=sample_size_use, curstrat=curstrat, outall = outall)
-    }
-    else if (method == "sys_eq"){
-      samptmp <- sys(frame=get(paste0("sort", i)), n=sample_size_use, curstrat=curstrat, outall = outall)
-    }
-    else if (method == "sys_pps"){
-      samptmp <- sys_pps(frame=get(paste0("sort", i)), n=sample_size_use, mos=mos, curstrat=curstrat, outall = outall)
-    }
-    else if (method == "chromy_pps"){
-      samptmp <- chromy_pps(frame=get(paste0("sort", i)), n=sample_size_use, mos=mos, curstrat=curstrat, outall = outall)
-    }
-
-    if (!is.null(strataog)){
-      samptmp <- strat_vals |>
-        cbind(samptmp)
-    }
-
-    assign(paste0("samp", i), samptmp)
-
-
-  } # End stratum loop
-
-  # Stack all samples together, then remove all sampi dataframes from workspace in case of future runs
-  sample <- tidytable::bind_rows(mget(paste0("samp", seq_len(nrow(strata_frames)))))
-
-
+  sampout <- strata_frames |>
+    tidytable::select(tidytable::all_of(strataog)) |>
+    tidytable::mutate(sample = samptmp) |>
+    data.table() |>
+    tidytable::unnest(sample)
 
   # Return final dataframe
-  return(sample)
-
+  return(sampout)
 }
